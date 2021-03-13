69102cfdaf66f4bb324bbc5d586533c6ba727b03
Rajinish0 <66392735+Rajinish0@users.noreply.github.com>
2021-03-14 01:02:30 +0500

    Update Terrain.py

--- Terrain.py
+++ Terrain.py
@@ -1,19 +1,20 @@
 import pygame,random,math,time,copy,sys,noise
-from seein import Vector, Matrix
+from Linalg import Vector, Matrix
 run = True
 w,h = 1000,680
-alteredW = 1200
-alteredH = 1200
+alteredW = 1200								## multiple of scl, for safe measures.
+alteredH = 1500
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((w,h))
 maxSpeed = 200
 frameRate = 30
 angleTurn = 10
-scl = 50
-angle = 0
+scl = 30
+flying = 0
 deltaTime = 1/frameRate
 orthographicProj = Matrix([[1,0,0],
-						               [0,1,0]])
+			   [0,1,0]])
+
 
 def CheckEvent():
 	global run
@@ -29,33 +30,26 @@ def drawLine(v1,v2, c= (255,0,0)):
 	wThresh,hThresh = w//2,h//2
 	pygame.draw.line(screen,c,(wThresh+x1,hThresh+y1),(wThresh+x2,hThresh+y2),2)
 
-def drawPlane(vert):
-	c = (255,0,0)
-	for i in range(1,len(vert)):
-		drawLine(vert[i-1],vert[i])
-	drawLine(vert[0],vert[-1])
 
-
-def Lerp(a,b,percent):
-	return (a+(b-a)*percent)
-
-
+## MAPS (-1,1) to a maximum limit so cur=-1 yeilds -maximum, 0 yields 0, 1 yeilds maximum, you get the idea
 def constrain(cur,maximum):
     return (((cur+1)*abs(maximum)*2)/2)-maximum
 
 def GetRandomZVals():
-	global yOff
+	global flying
 	terrain = []
+	yOff = copy.copy(flying)
 	for y in range(-alteredH//2,alteredH//2,scl*2):
 		t = []
-		xOff = 0
+		xOff =0
 		for x in range((-alteredW//2),alteredW//2,scl):
 			rNoise = noise.pnoise2(xOff,yOff)
-			rNoise= constrain(rNoise,300)
+			rNoise= constrain(rNoise,200)
 			t.append(rNoise)
 			xOff += .1
 		terrain.append(t)
-	yOff += .1
+		yOff += .1
+	flying -= .1
 	return terrain
 
 def GetVertices(pos,r,shape='SQUARE',z=0):
@@ -81,23 +75,25 @@ def GetMesh():
 		for x in range((-alteredW//2),alteredW//2,scl):
 			verts = [Vector(x,y,terrain[j][i]), Vector(x,y+scl,terrain[j][i])]
 			verts = [each.RotationX(60,Vector(0,0,0),return_new= False) for each in verts]
-			#verts = [each.applyTransformation(orthographicProj) for each in verts] ## DONT REALLY NEED TO APPLY THE ORTHOGRAPHIC PROJECTION
-                                      ##SINCE IM ONLY USING X AND Y FOR SHOWING THE VERTICES ON THE SCREEN
+			'''try random values, by all means.'''
+			orthographicProjs = [Matrix([[(1-(5-r.z/10)/100),0,0],
+						[0,(1-(5-r.z/10)/100),0]]) for r in verts]
+			verts = [each.applyTransformation(orthographicProjs[i]) for i,each in enumerate(verts)]
 			vertices.append(verts)
 			i += 1
 		j += 1
 	return vertices
 
 def DrawPiece(vertices):
-	c = (255,255,255)
+	c = (51,81,51)
 	for i,vert in enumerate(vertices):
 		drawLine(vertices[i-1],vert,c)
 	drawLine(vertices[0],vertices[-1],c)
-	drawLine(vertices[1],vertices[3],c)
-  
-  
+	drawLine(vertices[1],vertices[3],c) ## DIAGONAL
+
 def DrawMesh(Mesh):
 	for ind,vertices in enumerate(Mesh):
+		#VERY MESSY.
 		if ind % (alteredW//scl) != 0: 
 			DrawPiece(vertices+Mesh[ind-1][::-1])
 			if ind > alteredW//scl:
@@ -105,19 +101,15 @@ def DrawMesh(Mesh):
 				DrawPiece(nVert)
 
 
+		
+angle = .2
 
-def DrawLines(vert):
-	for i in range(4):
-		drawLine(vert[i],vert[(i+1)%4])
-		drawLine(vert[i+4],vert[((i+1)%4)+4])
-		drawLine(vert[i],vert[i+4])
-yOff = 0
+## COPY THESE DOWN OVER DRAWMESH to sort of fly over the terrain at 2 frames per second ;-;, gotta optimizie.
+terrain = GetRandomZVals()
+Mesh = GetMesh()
 
 while run:
 	screen.fill((0,0,0))
-	terrain = GetRandomZVals()
-	Mesh = GetMesh()
 	DrawMesh(Mesh)
-	yOff+=.001
 	pygame.display.update()
 	keys, run = CheckEvent()
