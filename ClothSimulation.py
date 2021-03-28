72f31295039f146b5e9fe812c2b9f4abc6d1b065
Rajinish0 <66392735+Rajinish0@users.noreply.github.com>
2021-03-28 18:47:33 +0500

    Update and rename Verlet.py to ClothSimulation.py

--- ClothSimulation.py
+++ ClothSimulation.py
@@ -8,14 +8,16 @@ alteredW = 1500
 alteredH = 1500
 screen = pygame.display.set_mode((w,h))
 clock = pygame.time.Clock()
-scale = 50
-depth = 150 ## Z depth for three d plane
-
+scl = 30
+## PLAY WITH THE VALUE OF GRAVITY..
+gravity = 0.2
 
 class Particle():
 	def __init__(self, pos=None,pinned=False):
 		self.pos = Vector(w//2+random.randint(-2,2),h//2+random.randint(-1,1)) if pos is None else pos
-		self.oldPos = Vector(self.pos.x+random.random()-5,self.pos.y+random.random()-5)
+		## FOR INIT VELOCITY
+		self.oldPos = Vector(self.pos.x+random.random()-7,self.pos.y) 
+		# self.oldPos = copy.copy(self.pos)
 		self.r = 3
 		self.pinned = pinned
 
@@ -25,7 +27,7 @@ class Particle():
 			self.vel = (self.pos-self.oldPos)*.999
 			self.oldPos = copy.copy(self.pos)
 			self.pos += self.vel
-			self.pos.y += .001
+			self.pos.y += gravity
 
 
 	@property
@@ -38,7 +40,8 @@ class Particle():
 	
 
 	def render(self):
-		pygame.draw.circle(screen,(255,255,255),(self.pos.x,self.pos.y),self.r)
+		pass
+		# pygame.draw.circle(screen,(255,255,255),(self.pos.x,self.pos.y),self.r)
 
 	def Edges(self):
 		vel = self.pos - self.oldPos
@@ -75,7 +78,7 @@ class PinnedParticle(Particle):
 	def update(self):
 		self.pos.x = self.basePos.x +  self.range*(math.cos(self.angle))
 		self.pos.y = self.basePos.y + self.range*(math.sin(self.angle))
-		self.angle += .001
+		self.angle += .005
 		PinnedParticle.update = Particle.update if not self.pinned else PinnedParticle.update
 
 	def render(self):
@@ -87,12 +90,11 @@ class Stick():
 	def __init__(self,p1,p2):
 		self.p1 = p1
 		self.p2 = p2
-		print(p1,p2)
-		self.length = dist(p1,p2)
+		self.length = dist(p1,p2)+2
 
 	def fixEndpoints(self):
-		dx = self.p2.x - self.p1.x
-		dy = self.p2.y - self.p1.y
+		dx = (self.p2.x - self.p1.x)
+		dy = (self.p2.y - self.p1.y)
 		disT = dist(self.p1,self.p2)
 		offSet = (self.length-disT)/disT/2
 		offSet = Vector(dx*offSet,dy*offSet)
@@ -104,8 +106,7 @@ class Stick():
 
 	def draw(self):
 		self.fixEndpoints()
-		pygame.draw.line(screen, (255,0,0), (self.p1.x,self.p1.y),(self.p2.x,self.p2.y),1)
-
+		pygame.draw.line(screen, (121,121,251), (self.p1.x,self.p1.y),(self.p2.x,self.p2.y),1)
 def dist(p1,p2):
 	return math.sqrt(pow(p1.x-p2.x,2)+pow(p1.y-p2.y,2))
 
@@ -120,12 +121,39 @@ def CheckEvent():
 			sys.exit()
 
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			ps[4].pinned = not ps[4].pinned
-			# print(ps[4].pinned)
+			mousePosx = pygame.mouse.get_pos()[0]
+			if mousePosx > w//2:
+				ps[0][-1].pinned = not ps[0][-1].pinned
+			else:
+				ps[0][0].pinned = not ps[0][0].pinned
 
 	keys = pygame.key.get_pressed()
 	return (keys,run)
 
+def GetMesh():
+	global scl,angle,terrain
+	vertices = []
+	## THESE VALUES ARE AD HOC..
+	for y in range(scl,h-300,20):
+		verts = []
+		for x in range(w//2-100,w//2 + w-1100,20):
+			verts.append(Particle(Vector(x,y)))
+		vertices.append(verts)
+	return vertices
+
+
+def getSticks(Mesh):
+	sticks = []
+	for i in range(len(Mesh)-1):
+		for j in range(len(Mesh[0])-1):
+			a = Mesh[i][j]
+			b = Mesh[i+1][j]
+			c = Mesh[i][j+1]
+			sticks.append(Stick(a,b))
+			sticks.append(Stick(a,c))
+	return sticks
+
+## A CHAINED CUBE
 
 ps = [
 Particle(Vector(w//2,h//2)),
@@ -150,11 +178,19 @@ Stick(ps[0],ps[2]),
 ]
 
 
+
 # ps = [Particle() for i in range(2)]
 # sticks = [Stick(*ps)]
+
+## CLOTH 
+ps = GetMesh()
+sticks = getSticks(ps)
+ps[0][0].pinned = True
+ps[0][-1].pinned = True
+
 def func():
 	screen.fill((0,0,0))
-	[(p.update(), p.render()) for p in ps]
+	[(p.update(), p.render()) for each in ps for p in each]
 	[stick.draw() for stick in sticks]
 	pygame.display.update()
 	CheckEvent()
